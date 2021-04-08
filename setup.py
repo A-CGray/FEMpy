523d200409b12e9f4df8d921377bb05ad47c7eaf
A-Gray-94 <alachris@umich.edu>
2021-04-08 14:58:44 -0400

    Updated dependencies

--- setup.py
+++ setup.py
@@ -3,26 +3,21 @@ import re
 
 __version__ = re.findall(
     r"""__version__ = ["']+([0-9\.]*)["']+""",
-    open('FEMpy/__init__.py').read(),
+    open("FEMpy/__init__.py").read(),
 )[0]
 
-setup(name='FEMpy',
-      version=__version__,
-
-
-      description="FEMpy is my attempt to implement a basic object oriented finite element method in python",
-      keywords='Finite Element Method, FEM',
-      author='Alasdair Christison Gray',
-      author_email='',
-      url='https://github.com/A-Gray-94/FEMpy',
-      license='Apache License Version 2.0',
-      packages=[
-        'FEMpy',
-      ],
-      install_requires=[
-            'numpy'
-      ],
-      classifiers=[
-        "Operating System :: OS Independent",
-        "Programming Language :: Python"]
-      )
+setup(
+    name="FEMpy",
+    version=__version__,
+    description="FEMpy is my attempt to implement a basic object oriented finite element method in python",
+    keywords="Finite Element Method, FEM",
+    author="Alasdair Christison Gray",
+    author_email="",
+    url="https://github.com/A-Gray-94/FEMpy",
+    license="Apache License Version 2.0",
+    packages=[
+        "FEMpy",
+    ],
+    install_requires=["numpy", "numba", "scipy"],
+    classifiers=["Operating System :: OS Independent", "Programming Language :: Python"],
+)
