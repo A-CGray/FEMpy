59f95f301c8543dcbf88357594e118036d4f8571
Alasdair Gray <alachris@umich.edu>
2021-04-16 16:17:15 -0400

    Change D Matrix to property

--- FEMpy/isoPlaneStrain.py
+++ FEMpy/isoPlaneStrain.py
@@ -26,13 +26,18 @@ from .Constitutive import Constitutive
 class isoPlaneStrain(Constitutive):
     def __init__(self, E, nu, t) -> None:
         super().__init__(numStrain=3, numDisp=2, numStress=3)
-        self.DMat = (
-            E
-            / ((1.0 - 2.0 * nu) * (1.0 + nu))
-            * np.array([[1.0 - nu, nu, 0.0], [nu, 1.0 - nu, 0.0], [0.0, 0.0, 0.5 * (1.0 - 2.0 * nu)]])
-        )
         self.LMats[0] = np.array([[1, 0], [0, 0], [0, 1]])
         self.LMats[1] = np.array([[0, 0], [0, 1], [1, 0]])
         self.E = E
         self.nu = nu
         self.t = t
+
+    @property
+    def DMat(self):
+        return (
+            self.E
+            / ((1.0 - 2.0 * self.nu) * (1.0 + self.nu))
+            * np.array(
+                [[1.0 - self.nu, self.nu, 0.0], [self.nu, 1.0 - self.nu, 0.0], [0.0, 0.0, 0.5 * (1.0 - 2.0 * self.nu)]]
+            )
+        )
--- FEMpy/isoPlaneStress.py
+++ FEMpy/isoPlaneStress.py
@@ -26,9 +26,16 @@ from .Constitutive import Constitutive
 class isoPlaneStress(Constitutive):
     def __init__(self, E, nu, t) -> None:
         super().__init__(numStrain=3, numDisp=2, numStress=3)
-        self.DMat = E / (1.0 - nu ** 2.0) * np.array([[1.0, nu, 0.0], [nu, 1.0, 0.0], [0.0, 0.0, 0.5 * (1.0 - nu)]])
         self.LMats[0] = np.array([[1, 0], [0, 0], [0, 1]])
         self.LMats[1] = np.array([[0, 0], [0, 1], [1, 0]])
         self.E = E
         self.nu = nu
         self.t = t
+
+    @property
+    def DMat(self):
+        return (
+            self.E
+            / (1.0 - self.nu ** 2.0)
+            * np.array([[1.0, self.nu, 0.0], [self.nu, 1.0, 0.0], [0.0, 0.0, 0.5 * (1.0 - self.nu)]])
+        )
