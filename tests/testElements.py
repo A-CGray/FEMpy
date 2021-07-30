550c25619aee37b97a499c2ce302955d3addf997
A-Gray-94 <alachris@umich.edu>
2021-07-29 23:47:37 -0400

    add first unit test file (tbc)

---
+++ tests/testElements.py
@@ -0,0 +1,33 @@
+"""
+==============================================================================
+Element unit tests
+==============================================================================
+@File    :   testElements.py
+@Date    :   2021/07/29
+@Author  :   Alasdair Christison Gray
+@Description :
+"""
+
+# ==============================================================================
+# Standard Python modules
+# ==============================================================================
+import unittest
+from parameterized import parameterized_class
+
+# ==============================================================================
+# External Python modules
+# ==============================================================================
+import numpy as np
+
+# ==============================================================================
+# Extension modules
+# ==============================================================================
+import FEMpy as fp
+
+# --- Elements to test: ---
+# QuadElement: 1st to 4th order
+# SerendipityQuad
+# Lagrange1DElement: 1st to 4th order
+
+if __name__ == "__main__":
+    unittest.main()
