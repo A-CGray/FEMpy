Lot's of fixes, residual computation is correctly returning zeros

FEMpy/Constitutive/NewIsoPlaneStress.py
@@ -21,7 +21,7 @@ import numpy as np
 # Extension modules
 # ==============================================================================
 from FEMpy.Constitutive.StrainModels import Planar2DStrain, Planar2DStrainSens
-from FEMpy.Constitutive.StressModels import isoPlaneStress, isoPlaneStressStrainSens
+from FEMpy.Constitutive.StressModels import isoPlaneStress, isoPlaneStressStrainSens, vonMises2DPlaneStress
 from FEMpy.Constitutive import ConstitutiveModel
 
 
@@ -59,11 +59,15 @@ class IsoPlaneStress(ConstitutiveModel):
         stressNames = ["sigma_xx", "sigma_yy", "tau_xy"]
 
         # --- Functions ---
-        functionNames = ["Mass", "Von Mises Stress", "Tresca Stress"]
+        functionNames = ["Mass", "Von Mises Stress"]
+
+        # --- Material properties ---
+        self.E = E
+        self.nu = nu
 
         numDim = 2
 
-        self.super.__init__(numDim, stateNames, strainNames, stressNames, designVars, functionNames, linear)
+        super().__init__(numDim, stateNames, strainNames, stressNames, designVars, functionNames, linear)
 
     def computeStrains(self, states, stateGradients, coords, dvs):
         """Given the coordinates, state value, state gradient, and design variables at a bunch of points, compute the strains at each one
@@ -86,7 +90,7 @@ class IsoPlaneStress(ConstitutiveModel):
         numPoints x numStrains array
             Strain components at each point
         """
-        return Planar2DStrain(UPrime=stateGradients, nonlinear=not self.linear)
+        return Planar2DStrain(UPrime=stateGradients, nonlinear=not self.isLinear)
 
     def computeStrainStateGradSens(self, states, stateGradients, coords, dvs):
         """Given the coordinates, state value, state gradient, and design variables at a bunch of points, compute the
@@ -110,7 +114,7 @@ class IsoPlaneStress(ConstitutiveModel):
         numPoints x numStrains x numStates x numDim array
             Strain sensitivities, sens[i,j,k,l] is the sensitivity of strain component j at point i to state gradient du_k/dx_l
         """
-        return Planar2DStrainSens(UPrime=stateGradients, nonlinear=not self.linear)
+        return Planar2DStrainSens(UPrime=stateGradients, nonlinear=not self.isLinear)
 
     def computeStresses(self, strains, dvs):
         """Given the strains and design variables at a bunch of points, compute the stresses at each one
@@ -168,3 +172,41 @@ class IsoPlaneStress(ConstitutiveModel):
             Volume scaling parameter at each point
         """
         return dvs["Thickness"]
+
+    def getFunction(self, name):
+        """Return a function that can be computed for this constitutive model
+
+        Parameters
+        ----------
+        name : str
+            Name of the function to compute
+
+        Returns
+        -------
+        callable
+            A function that can be called to compute the desired function at a bunch of points with the signature, f(states, stateGradients, coords, dvs)
+            where:
+            states is a numPoints x numStates array
+            stateGradients is a numPoints x numStates x numDim array
+            coords is a numPoints x numDim array
+            dvs is a dictionary of numPoints length arrays
+        """
+        lowerCaseFuncNames = [func.lower() for func in self.functionNames]
+        if name.lower() not in self.lowerCaseFuncNames:
+            raise ValueError(
+                f"{name} is not a valid function name for this constitutive model, valid choices are {self.functionNames}"
+            )
+
+        if name.lower() == "mass":
+
+            def massFunc(states, stateGradients, coords, dvs):
+                return np.ones(states.shape[0]) * self.rho
+
+            return massFunc
+
+        if name.lower() == "von mises stress":
+
+            def VMFunc(states, stateGradients, coords, dvs):
+                strains = self.computeStrains(states, stateGradients, coords, dvs)
+                stresses = self.computeStresses(strains, dvs)
+                return vonMises2DPlaneStress(stresses)

FEMpy/Elements/NewQuadElement.py
@@ -40,7 +40,7 @@ class QuadElement2D(Element):
         self.order = order
         numNodes = (order + 1) ** 2
         if quadratureOrder is None:
-            shapeFuncOrder = order**2
+            shapeFuncOrder = 2 * order
             quadratureOrder = int(np.ceil((shapeFuncOrder + 1) / 2))
         super().__init__(numNodes, numDimensions=2, numStates=numStates, quadratureOrder=quadratureOrder)
 
@@ -102,7 +102,7 @@ class QuadElement2D(Element):
         """
         if order is None:
             order = self.quadratureOrder
-        return getGaussQuadWeights(order)
+        return getGaussQuadWeights(self.numDim, order)
 
     def getIntegrationPointCoords(self, order=None):
         """Compute the integration point parameteric coordinates for a given quadrature order on this element
@@ -119,7 +119,7 @@ class QuadElement2D(Element):
         """
         if order is None:
             order = self.quadratureOrder
-        return getGaussQuadWeights(order)
+        return getGaussQuadPoints(self.numDim, order)
 
     def getReferenceElementCoordinates(self):
         """Get the node coordinates for the reference element, a.k.a the element on which the shape functions are defined