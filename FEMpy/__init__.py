2191d0b3b28ce0ce45c7b20040f1b33e8b024899
Alasdair Gray <alachris@umich.edu>
2022-12-05 21:30:55 -0500

    Remove unneeded mesh processing stuff

--- FEMpy/__init__.py
+++ FEMpy/__init__.py
@@ -6,7 +6,6 @@ from . import Quadrature
 from . import Constitutive
 from . import Basis
 from . import Elements
-from . import Mesh
 from . import Utils
 from .Problem import FEMpyProblem
 from .Model import FEMpyModel
