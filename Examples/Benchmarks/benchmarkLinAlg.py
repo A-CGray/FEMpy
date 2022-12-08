7a3ea7d665d6c3088a97ecb273515d558c558ed4
Alasdair Gray <alachris@umich.edu>
2022-12-08 17:58:46 -0500

    Add CI jobs for benchmarks and examples

--- Examples/Benchmarks/benchmarkLinAlg.py
+++ Examples/Benchmarks/benchmarkLinAlg.py
@@ -22,24 +22,29 @@ import timeit
 # Extension modules
 # ==============================================================================
 
-setupString = """
+numMats = 80000
+setupString = f"""
 import numpy as np
 from FEMpy.LinAlg import det2, det3, inv2, inv3
 
-A2 = np.random.rand(400, 2, 2)
-A3 = np.random.rand(800, 3, 3)
+A2 = np.random.rand({numMats}, 2, 2)
+A3 = np.random.rand({numMats}, 3, 3)
 det2(A2)
 det3(A3)
 inv2(A2)
 inv3(A3)
 """
-for func in ["det", "inv"]:
-    for size in [2, 3]:
-        print("\n")
-        for method in ["numpy", "numba"]:
-            if method == "numpy":
-                test = f"np.linalg.{func}(A{size})"
-            else:
-                test = f"{func}{size}(A{size})"
-            t = timeit.timeit(test, setup=setupString, number=50000)
-            print(f"Time to compute {size} x {size} {func}s with {method} = {t:7.11e} s")
+N = 50
+
+
+def benchmark_LinAlg():
+    for func in ["det", "inv"]:
+        for size in [2, 3]:
+            print("\n")
+            for method in ["numpy", "numba"]:
+                if method == "numpy":
+                    test = f"np.linalg.{func}(A{size})"
+                else:
+                    test = f"{func}{size}(A{size})"
+                t = timeit.timeit(test, setup=setupString, number=N) / N
+                print(f"Time to compute {numMats} {size} x {size} {func}s with {method} = {t:7.11e} s")
